350ab424c812d2b5647c5b9d45ca6fba38f6c382
elbenis <elb3nis@proton.me>
2026-04-03 20:32:28 +0100

    Add basic contributor documentation
    Signed-off-by: elbenis <elb3nis@proton.me>

--- docs/contributor-doc/astro.config.mjs
+++ docs/contributor-doc/astro.config.mjs
@@ -8,16 +8,15 @@ export default defineConfig({
 		starlight({
 			title: 'Sarian Docs',
 			logo: {
-				light: './src/assets/SarianLogo.webp',
-				dark: './src/assets/SarianLogo.webp',
+				light: './src/assets/SarianLogo.png',
+				dark: './src/assets/SarianLogo.png',
 			},
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Contributing Guide', slug: 'guides/contributing_guide' },
 					],
 				},
 				{
---
+++ docs/contributor-doc/src/content/docs/guides/contributing_guide.md
@@ -0,0 +1,6 @@
+---
+title: Contributing Guide
+description: Contributing Guide
+---
+
+**You can find all the contributing guidelines at ```CONTRIBUTING.md``` and ```CODE_STYLE.md```.**
